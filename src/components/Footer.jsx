f615bc554bfcb3eaf2c5381899bb9457cfcba775
AbdoulayeAbdoulaye Alhassane Diallo <alhassaneabdoulaye@gmail.com>
2025-12-09 12:36:44 -0500

    complete the landingPage

---
+++ src/components/Footer.jsx
@@ -0,0 +1,53 @@
+import logo from "../assets/logo.svg";
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <div className="footer-content">
+        <div className="footer-section">
+          <img src={logo} alt="Columbia Cribs Logo" />
+          <h1>
+            Columbia<span>Cribs</span>
+          </h1>
+        </div>
+        <div className="footer-section">
+          <h4>Quick Links</h4>
+          <ul>
+            <li>
+              <a href="#home">Home</a>
+            </li>
+            <li>
+              <a href="#buildings">Buildings</a>
+            </li>
+            <li>
+              <a href="#about">About</a>
+            </li>
+            <li>
+              <a href="#contact">Contact</a>
+            </li>
+          </ul>
+        </div>
+        <div className="footer-section">
+          <h4>Connect</h4>
+          <ul>
+            <li>
+              <a href="#facebook">Facebook</a>
+            </li>
+            <li>
+              <a href="#instagram">Instagram</a>
+            </li>
+            <li>
+              <a href="#twitter">Twitter</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className="footer-bottom">
+        <p>&copy; 2024 Columbia Cribs. All rights reserved.</p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
