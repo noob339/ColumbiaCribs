d6446808ba339a647240e8f33735e3a38869c180
Navil Talukdar <naviltalukdar3@gmail.com>
2025-12-14 15:19:34 -0500

    file/folder reorganization

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import "./App.css";
 import Header from "./components/Header.jsx";
 import Footer from "./components/Footer.jsx";
-import Home from "./Pages/Home.jsx"
+import Home from "./pages/Home.jsx"
 
 import ReviewPage from "./pages/ReviewPage.jsx";
-import VerifyEmail from "./components/VerifyEmail.jsx";
+import VerifyEmail from "./pages/VerifyEmail.jsx";
 import EnterCode from "./components/EnterCode.jsx";
-import Success from "./components/Success.jsx";
+import Success from "./pages/Success.jsx";
 
 import { Routes, Route } from "react-router-dom";
 
