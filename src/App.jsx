8cbe9e521393259fb458d19c0db93b5f25620775
Navil Talukdar <naviltalukdar3@gmail.com>
2025-12-14 15:09:19 -0500

    file/folder reorganization

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import "./App.css";
 import Header from "./components/Header.jsx";
 import Footer from "./components/Footer.jsx";
-import Home from "./components/Home.jsx";
+import Home from "./Pages/Home.jsx"
 
-import ReviewPage from "./components/ReviewPage.jsx";
+import ReviewPage from "./pages/ReviewPage.jsx";
 import VerifyEmail from "./components/VerifyEmail.jsx";
 import EnterCode from "./components/EnterCode.jsx";
 import Success from "./components/Success.jsx";
