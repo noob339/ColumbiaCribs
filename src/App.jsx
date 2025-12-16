95c0e9fe375f287641d4bfb9590a2529589b875b
noob339 <88294617+noob339@users.noreply.github.com>
2025-12-16 18:46:53 -0500

    error on Home.jsx, it had Pages as the dir instead of pages, weird because I did not edit this

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import "./App.css";
 import Header from "./components/Header.jsx";
 import Footer from "./components/Footer.jsx";
-import Home from "./Pages/Home.jsx";
+import Home from "./pages/Home.jsx";
 
 import ReviewPage from "./pages/ReviewPage.jsx";
 import VerifyEmail from "./pages/VerifyEmail.jsx";
