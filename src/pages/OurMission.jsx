ca3d9d9b642e7a1e3d4962bb32a1671a9ef5018c
Navil Talukdar <naviltalukdar3@gmail.com>
2025-12-28 00:02:27 -0500

    Our Mission page complete with mock mission

--- src/pages/OurMission.jsx
+++ src/pages/OurMission.jsx
@@ -1,11 +1,30 @@
 import React from 'react'
+import './OurMission.css'
 
 function OurMission() {
-  return (
-    <div>
-      
-    </div>
-  )
+    return (
+        <div className="mission-container">
+          <h1 className="mission-title">OurMission</h1>
+          
+          <div className="mission-content">
+            <h2 className="mission-subtitle">About Us</h2>
+            <p className="mission-text">
+              ABOUT US ABOUT US ABOUT US ABOUT US ABOUT US ABOUT US ABOUT US ABOUT US ABOUT US ABOUT US ABOUT 
+              US ABOUT US ABOUT US ABOUT US ABOUT US ABOUT US ABOUT US ABOUT US ABOUT US ABOUT US ABOUT US ABOUT 
+              US ABOUT US ABOUT US ABOUT US ABOUT US ABOUT US ABOUT US ABOUT US ABOUT US ABOUT US ABOUT US ABOUT US 
+            </p>
+          </div>
+          <div className="mission-content">
+            <h2 className="mission-subtitle">Our Goal</h2>
+            <p className="mission-text">
+              OUR GOAL OUR GOAL OUR GOAL OUR GOAL OUR GOAL OUR GOAL OUR GOAL OUR GOAL OUR GOAL OUR GOAL OUR GOAL 
+              OUR GOAL OUR GOAL OUR GOAL OUR GOAL OUR GOAL OUR GOAL OUR GOAL OUR GOAL OUR GOAL OUR GOAL 
+              OUR GOAL OUR GOAL OUR GOAL OUR GOAL OUR GOAL OUR GOAL OUR GOAL OUR GOAL OUR GOAL OUR GOAL OUR GOAL 
+            </p>
+          </div>
+        </div>
+      );
+    
 }
 
 export default OurMission
